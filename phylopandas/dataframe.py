a4c94d5cac737416a57add8d78899c4877d8ff1b
Zachary Sailer <zachsailer@gmail.com>
2017-10-24 21:08:54 -0700

    add paml reader

--- phylopandas/dataframe.py
+++ phylopandas/dataframe.py
@@ -1,4 +1,5 @@
 import os
+import re 
 
 from Bio import SeqIO
 from Bio.Seq import Seq
@@ -64,6 +65,54 @@ def read_nexus(filename, **kwargs):
 def read_swiss(filename, **kwargs):
     """Read Swiss-Prot aka UniProt format."""
     return _read(filename, format='nexus', **kwargs)
+
+def read_paml(filename, **kwargs):
+    """Read PAML output and get ancestors as DataFrame. Returns DataFrame and tree as a string.
+    """
+    # Read paml output.
+    with open(filename, 'r') as f:
+        data = f.read()
+    
+    # Match tree with ancestor labels
+    regex = re.compile("tree with node labels for Rod Page's TreeView\n.+\n")
+    match = regex.search(data)
+    tree = match.group().split("\n")[1]
+
+    # Initialize the ancestor_data dictionary
+    ancestor_data = {}
+
+    # Compile a regular expression to find blocks of data for internal nodes
+    node_regex = re.compile("""Prob distribution at node [0-9]+, by site[-\w():.\s]+\n\n""")
+    # Strip the node number from this block of data.
+    node_num_regex = re.compile("[0-9]+")
+
+    # Iterate through each block of internal node data
+    index, sequences, posteriors = [], [], []
+    for node in node_regex.findall(data):
+        # Initialize a dictionary for site data
+        site_data = {}
+
+        # Compile regex for matching site data
+        site_regex = re.compile("(?:\w\(\w.\w{3}\) )+")
+
+        site_num = 0
+        # Iterate through each match for site data.
+        seq, post = [], []
+        for site in site_regex.findall(node):
+            # Iterate through residues
+            scores = [float(site[i+2:i+7]) for i in range(0,len(site), 9)]
+            j, p = max(enumerate(scores), key=lambda item: item[1])
+            seq.append(site[j*9])
+            post.append(p)
+
+        # Add site data to ancestor_data
+        index.append(int(node_num_regex.search(node).group(0))) # Get node_number defined by PAML
+        sequences.append("".join(seq))
+        posteriors.append(sum(post)/len(post))
+        
+    df = DataFrame({"sequence":sequences, "posterior":posteriors}, index=index)
+    return tree, df
+
 
 class DataFrame(pd.DataFrame):
     
