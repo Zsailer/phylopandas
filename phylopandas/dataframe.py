3810338817b5b102d8374ebc477399bb31a1d705
Zachary Sailer <zachsailer@gmail.com>
2017-10-25 10:45:02 -0700

    allow writing to string

--- phylopandas/dataframe.py
+++ phylopandas/dataframe.py
@@ -7,7 +7,7 @@ from Bio.SeqRecord import SeqRecord
 
 import pandas as pd
 
-def _read(filename, format, seq_label='sequence', **kwargs):
+def _read(filename, schema, seq_label='sequence', **kwargs):
     """Use BioPython's sequence parsing module to convert any file format to 
     a Pandas DataFrame.
     """
@@ -15,7 +15,7 @@ def _read(filename, format, seq_label='sequence', **kwargs):
     data = {'id':[], seq_label:[], 'description':[], 'name':[], 'db_id':[]}
     
     # Parse Fasta file.
-    for i, s in enumerate(SeqIO.parse(filename, format=format, **kwargs)):
+    for i, s in enumerate(SeqIO.parse(filename, format=schema, **kwargs)):
         data['db_id'].append(s.id)
         data[seq_label].append(str(s.seq))
         data['description'].append(s.description)
@@ -25,7 +25,7 @@ def _read(filename, format, seq_label='sequence', **kwargs):
     # Port to DataFrame.
     return DataFrame(data)
 
-def _write(dataframe, filename, format, sequence_col='sequence', id_only=False, **kwargs):
+def _write(dataframe, filename=None, schema='fasta', sequence_col='sequence', id_only=False, **kwargs):
     """Write a PhyloPandas DataFrame to a sequence file.
     """
     seq_records = []
@@ -34,20 +34,16 @@ def _write(dataframe, filename, format, sequence_col='sequence', id_only=False, 
         seq = Seq(row[sequence_col])
         # Create a SeqRecord and append to list.
         if id_only:
-            seq_records.append(SeqRecord(
-                seq,
-                id=row['id'],
-                name='',
-                description=''))
+            record = SeqRecord(seq, id=row['id'], name='', description='')
         else:
-            seq_records.append(SeqRecord(
-                seq,
-                id=row['id'],
-                name=row['name'],
-                description=row['description']))
+            record = SeqRecord(seq, id=row['id'], name=row['name'], description=row['description'])
+        seq_records.append(record)
             
-    # Write out sequences        
-    SeqIO.write(seq_records, filename, format=format, **kwargs)
+    # Write to disk or return string
+    if filename != None:  
+        SeqIO.write(seq_records, filename, format=format, **kwargs)
+    else:
+        return "".join([s.format(schema) for s in seq_records])
 
 def read_fasta(filename, **kwargs):
     """Read fasta format."""
@@ -76,29 +72,29 @@ def read_swiss(filename, **kwargs):
 
 class DataFrame(pd.DataFrame):
     
-    def to_fasta(self, f, sequence_col='sequence', id_only=True):
+    def to_fasta(self, filename=None, sequence_col='sequence', id_only=True):
         """Write to fasta format."""
-        _write(self, f, format="fasta", id_only=True)
+        return _write(self, filename=filename, schema="fasta", id_only=True)
         
-    def to_phylip(self, f, sequence_col='sequence'):
+    def to_phylip(self, filename=None, sequence_col='sequence'):
         """Write to phylip format."""
-        _write(self, f, format="phylip")
+        return _write(self, filename=filename, schema="phylip")
 
-    def to_clustal(self, f, sequence_col='sequence'):
+    def to_clustal(self, filename=None, sequence_col='sequence'):
         """Write to alignment format of Clustal X and Clustal W."""
-        _write(self, f, format="clustal")
+        return _write(self, filename=filename, schema="clustal")
         
-    def to_embl(self, f, sequence_col='sequence'):
+    def to_embl(self, filename=None, sequence_col='sequence'):
         """Write to the EMBL flat file format."""
-        _write(self, f, format="embl")
+        return _write(self, filename=filename, schema="embl")
 
-    def to_nexus(self, f, sequence_col='sequence'):
+    def to_nexus(self, filename=None, sequence_col='sequence'):
         """Write to the NEXUS multiple alignment format."""
-        _write(self, f, format="nexus")
+        return _write(self, filename=filename, schema="nexus")
         
-    def to_swiss(self, f, sequence_col='sequence'):
+    def to_swiss(self, filename=None, sequence_col='sequence'):
         """Write Swiss-Prot aka UniProt format."""
-        _write(self, f, format="swiss")
+        return _write(self, filename=filename, schema="swiss")
         
     
     
